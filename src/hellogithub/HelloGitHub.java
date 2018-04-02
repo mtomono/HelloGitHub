6d49eb4db29c3aa82d6b3e0d5036f6bd4a840a8e
Masao Tomono <mtomono@acm.org>
2018-04-02 16:30:55 +0900

    added README

---
+++ src/hellogithub/HelloGitHub.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellogithub;
+
+/**
+ *
+ * @author masao
+ */
+public class HelloGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("GitHub");
+    }
+    
+    public String get() {
+        return "GitHub";
+    }
+    
+}
